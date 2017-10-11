00cecb251cba3b1b9e69cd1b5a25596310c2ff70
suvar3_7 <suvar3_7@mac.com>
2017-10-11 15:02:03 -0400

    initial commit

---
+++ db/migrate/20171009204230_create_ideas.rb
@@ -0,0 +1,11 @@
+class CreateIdeas < ActiveRecord::Migration[5.1]
+  def change
+    create_table :ideas do |t|
+      t.string :name
+      t.text :description
+      t.string :pictures
+
+      t.timestamps
+    end
+  end
+end
